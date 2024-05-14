68702bfacef46c68ba510c39fac1ca35af489bc6
Ubuntu <ubuntu@ip-10-192-11-125.us-east-2.compute.internal>
2024-05-14 22:23:50 +0000

    edit word-count

--- Lab6/word-count/wc.py
+++ Lab6/word-count/wc.py
@@ -1,12 +1,13 @@
 from pyspark import SparkContext, SparkConf
 
-DATA = "./data/sh.txt"
-OUTPUT_DIR = "counts_sh" # name of the folder
+DATA = "./data/*.txt"
+OUTPUT_DIR = "counts" # name of the folder
 
 def word_count():
     sc = SparkContext("local","Word count example")
     textFile = sc.textFile(DATA)
     counts = textFile.flatMap(lambda line: line.split(" ")).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
-    counts.saveAsTextFile(OUTPUT_DIR)
+    filtered_counts = counts.filter(lambda x: x[1] >= 3)
+    filtered_counts.coalesce(1).saveAsTextFile(OUTPUT_DIR)
     print("Number of partitions: ", textFile.getNumPartitions())
 word_count()
